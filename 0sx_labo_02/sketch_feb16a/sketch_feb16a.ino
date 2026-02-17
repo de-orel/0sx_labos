2b37d0a41309403d4701787c7a22831e973b8772
de-orel <tresornguedia194@gmail.com>
2026-02-16 23:11:04 -0500

    première version

--- 0sx_labo_02/sketch_feb16a/sketch_feb16a.ino
+++ 0sx_labo_02/sketch_feb16a/sketch_feb16a.ino
@@ -1,9 +1,82 @@
-void setup() {
-  // put your setup code here, to run once:
+const int ledPins[] = {7, 8, 9, 10};
+int potentiometerPin = A1;
+int potentiometerValue = 0;
+const int bouton = 2;
 
+const unsigned long debounceDelay = 50;
+unsigned long lastDebounceTime = 0;
+int lastButtonState = HIGH;
+int buttonState;
+
+void setup() {
+  Serial.begin(9600);
+  for (int i = 0; i < 4; i++){
+    pinMode(ledPins[i], OUTPUT);
+  }
+  pinMode(bouton, INPUT_PULLUP);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  potentiometerValue = analogRead(potentiometerPin);
+  int converted_value = map(potentiometerValue, 0, 1023, 0, 20);
+  int pourcentage = map(converted_value, 0, 20, 0, 100);
 
+  showLevel(converted_value, pourcentage);
+  printLeds(pourcentage);
+}
+
+
+void showLevel(int converted_value, int pourcentage){
+
+  int reading = digitalRead(bouton);
+
+  if (reading != lastButtonState){
+    lastDebounceTime = millis();
+  }
+  if (millis() - lastDebounceTime >= debounceDelay){
+    if(reading != buttonState){
+      buttonState = reading;
+    
+      if (digitalRead(bouton) == 0){
+        Serial.print("[");
+        for(int i = 0; i < converted_value; i++){
+          Serial.print("!");
+        }
+        for (int i = 0; i < (20 - converted_value); i++){
+          Serial.print(".");
+        }
+        Serial.println("]   " + String(pourcentage) + "%");
+      }
+    }
+  }
+  lastButtonState = reading;
+}
+
+void printLeds(int pourcentage){
+
+    if (pourcentage <= 25){
+    digitalWrite(ledPins[0], HIGH);
+    digitalWrite(ledPins[1], LOW);
+    digitalWrite(ledPins[2], LOW);
+    digitalWrite(ledPins[3], LOW);
+    }
+  else if(pourcentage <= 50){
+    digitalWrite(ledPins[0], HIGH);
+    digitalWrite(ledPins[1], HIGH);
+    digitalWrite(ledPins[2], LOW);
+    digitalWrite(ledPins[3], LOW);
+  }
+  else if(pourcentage <= 75){  
+    digitalWrite(ledPins[0], HIGH);
+    digitalWrite(ledPins[1], HIGH);
+    digitalWrite(ledPins[2], HIGH);
+    digitalWrite(ledPins[3], LOW);
+  }
+  else {
+    digitalWrite(ledPins[0], HIGH);
+    digitalWrite(ledPins[1], HIGH);
+    digitalWrite(ledPins[2], HIGH);
+    digitalWrite(ledPins[3], HIGH);
+  }
 }
